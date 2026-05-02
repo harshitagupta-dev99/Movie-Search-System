57c305385579b7254c77b89402ecc61de56afade
Harshita <hgupta5_be18@thapar.edu>
2026-05-02 13:33:18 +0530

    display movies api endpoint updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,29 @@
-const API_KEY = "";
-const BASE_URL = "https://api.themoviedb.org/3";
+const API_TOKEN = import.meta.env.VITE_API_TOKEN;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const BASE_URL = "https://api.themoviedb.org/3/movie/popular";
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const options = {
+    method: "GET",
+     headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_TOKEN}`
+  }
+  };
+  const response = await fetch(
+    `${BASE_URL}?language=en-US&page=1`,
+    options,
+  );
   const data = await response.json();
   return data.results;
 };
 
-export const searchMovies = async (query) => {
+export const searchMovies = async (query) => { //needs to be updated with token .
   const response = await fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
-      query
-    )}`
+      query,
+    )}`,
   );
   const data = await response.json();
   return data.results;
